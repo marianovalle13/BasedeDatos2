3d3f066fb3a508e65fa88cb92538bcd9a463c5e3
marianovalle13 <marianovalle13@gmail.com>
2018-04-19 11:27:49 -0300

    asd

--- clase_4.sql
+++ clase_4.sql
@@ -6,9 +6,9 @@ SELECT DISTINCT length FROM film;
 
 SELECT title, rental_rate, replacement_cost FROM film WHERE replacement_cost BETWEEN 20 AND 24;
 
-SELECT film.title, category.name, film.rating FROM film, film_category, category WHERE film.film_id = film_category.film_id AND film_category.category_id = category.category_id AND film.special_features = "Behind the Scenes";
+SELECT film.title, category.name, film.rating FROM film, film_category, category WHERE film.film_id = film_category.film_id AND film_category.category_id = category.category_id AND film.special_features = "BEHIND THE SCENES";
 
-SELECT first_name, last_name from actor, film, film_actor WHERE actor.actor_id = film_actor.actor_id AND film_actor.film_id = film.film_id AND film.title = "ZOOLANDER FICTION";
+SELECT first_name, last_name from actor, film, film_actor WHERE actor.actor_id = film_actor.actor_id AND film_actor.film_id = film.film_id AND film.title = "ZOOLANDER FACTION";
 
 SELECT address.address, city.city, country.country FROM store, address, city, country WHERE store.address_id = address.address_id AND address.city_id = city.city_id AND city.country_id = country.country_id AND store.store_id = 1;
 
